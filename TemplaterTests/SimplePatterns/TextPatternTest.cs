d5927247e9e5d78c810e8d23e63635682911dc09
FrostyMemes <lager11197@yandex.ru>
2022-12-15 16:21:48 +0300

    fix: formated tests for simple text pattern

--- TemplaterTests/SimplePatterns/TextPatternTest.cs
+++ TemplaterTests/SimplePatterns/TextPatternTest.cs
@@ -3,10 +3,11 @@ using Templater.Patterns;
 namespace TemplaterTests;
 
 [TestClass]
-public class SimplePatternsTest
+public class TextPatternTest
 {
+    
     [TestMethod]
-    public void TextPatternSuccesseful()
+    public void TextPatternSuccessefulFinding()
     {
         var testText = "TestText";
         var text = "Test";
@@ -16,6 +17,33 @@ public class SimplePatternsTest
         var patternResult = testTextPattern.Execute(testText, posititon);
         Assert.IsNotNull(patternResult);
     }
+    
+    
+    [TestMethod]
+    public void TextPatternUnsuccessefulFinding()
+    {
+        var testText = "TestText";
+        var text = "WTest";
+        var posititon = 0;
+        
+        var testTextPattern = new TextPattern(text);
+        var patternResult = testTextPattern.Execute(testText, posititon);
+        Assert.IsNull(patternResult);
+    }
+    
+    
+    [TestMethod]
+    public void TextPatternSuccessefulCheckFindingResult()
+    {
+        var testText = "TestText";
+        var text = "Test";
+        var posititon = 0;
+        
+        var testTextPattern = new TextPattern(text);
+        var patternResult = testTextPattern.Execute(testText, posititon);
+        Assert.AreEqual("Test",patternResult.Result);
+    }
+    
     
     [TestMethod]
     public void TextPatternSuccessefulCheckEndPosition()
@@ -27,45 +55,19 @@ public class SimplePatternsTest
         var testTextPattern = new TextPattern(text);
         var patternResult = testTextPattern.Execute(testText, posititon);
         
-        var expectation = 4;
-        
-        Assert.AreEqual(expectation, patternResult.EndPosition);
+        Assert.AreEqual(4, patternResult.EndPosition);
     }
     
-    [TestMethod]
-    public void TextPatternUnsuccesseful()
-    {
-        var testText = "TestText";
-        var text = "Untest";
-        var posititon = 0;
-        
-        var testTextPattern = new TextPattern(text);
-        var patternResult = testTextPattern.Execute(testText, posititon);
-        Assert.IsNull(patternResult);
-    }
     
     [TestMethod]
     public void TextPatternTryFindWithWrongStartPosition()
     {
         var testText = "TestText";
         var text = "Test";
-        var posititon = 1;
+        var posititon = 4;
         
         var testTextPattern = new TextPattern(text);
         var patternResult = testTextPattern.Execute(testText, posititon);
         Assert.IsNull(patternResult);
     }
-    
-    
-    // [TestMethod]
-    // public void RegexPatternSuccessefulFinding()
-    // {
-    //     
-    // }
-    //
-    // [TestMethod]
-    // public void RegexPatternUnsuccessefulFinding()
-    // {
-    //     
-    // }
 }
