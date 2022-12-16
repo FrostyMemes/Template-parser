f882826d5fbbbb475688699e47d74fb37553cbec
FrostyMemes <lager11197@yandex.ru>
2022-12-16 16:16:08 +0300

    add: tests for new combinator pattern AnyPattern

---
+++ TemplaterTests/CombinatorPatterns/AnyPatternTest.cs
@@ -0,0 +1,61 @@
+﻿using Templater.Patterns;
+using Templater.Patterns.Combinators;
+
+namespace TemplaterTests.CombinatorPatterns;
+
+[TestClass]
+
+public class AnyPatternTest
+{
+    [TestMethod]
+    public void SuccessefulFindingWithTwoPatternsWhenSuccessefulFirstPattern()
+    {
+        var testText = "TestText";
+        var text1 = "Test";
+        var text2 = "West";
+        var posititon = 0;
+        
+        var textPatternSuccesseful = new TextPattern(text1);
+        var textPatternUnsuccesseful = new TextPattern(text2);
+        var testAnyPattern = new AnyPattern(textPatternSuccesseful, textPatternUnsuccesseful);
+        
+        var patternResult = testAnyPattern.Execute(testText, posititon);
+        
+        Assert.IsNotNull(patternResult);
+    }
+    
+    [TestMethod]
+    public void SuccessefulFindingWithTwoPatternsWhenSuccessefulSecondPattern()
+    {
+        var testText = "TestText";
+        var text1 = "Test";
+        var text2 = "West";
+        var posititon = 0;
+        
+        var textPatternSuccesseful = new TextPattern(text1);
+        var textPatternUnsuccesseful = new TextPattern(text2);
+        var testAnyPattern = new AnyPattern(textPatternUnsuccesseful, textPatternSuccesseful);
+        
+        var patternResult = testAnyPattern.Execute(testText, posititon);
+        
+        Assert.IsNotNull(patternResult);
+    }
+    
+    [TestMethod]
+    public void CorrectFindingResult()
+    {
+        var testText = "TestText";
+        var text1 = "Test";
+        var text2 = "West";
+        var posititon = 0;
+        
+        var textPatternSuccesseful = new TextPattern(text1);
+        var textPatternUnsuccesseful = new TextPattern(text2);
+        var testAnyPattern = new AnyPattern(textPatternUnsuccesseful, textPatternSuccesseful);
+        
+        var patternResult = testAnyPattern.Execute(testText, posititon);
+        
+        Assert.AreEqual(text1, patternResult.Result);
+    }
+    
+}
