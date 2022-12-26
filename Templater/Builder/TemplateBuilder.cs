99183735fbdd45299b77ac6b0c94a28f38620695
anatoly <lager11197@yandex.ru>
2022-12-26 23:35:35 +0300

    feat: add methods for collections of attributes of tag

--- Templater/Builder/TemplateBuilder.cs
+++ Templater/Builder/TemplateBuilder.cs
@@ -4,28 +4,42 @@ namespace Templater.Builder;
 
 public class TemplateBuilder
 {
-     private StringBuilder template;
+    private readonly StringBuilder template;
 
-     public TemplateBuilder() => template = new StringBuilder(100);
-     public TemplateBuilder(string head) => template = new StringBuilder(head, 100);
-     public string Build() => template.ToString();
+    public TemplateBuilder() => template = new StringBuilder(100);
+    public TemplateBuilder(string head) => template = new StringBuilder(head, 100);
+    public string Build() => template.ToString();
+    
+    public TemplateBuilder AddTag(string tag)
+    {
+        template.Append($"<{tag}>\n");
+        return this;
+    }
 
-     public TemplateBuilder AddTag(string tag)
-     {
-          template.Append($"<{tag}>\n");
-          return this;
-     }
+    public TemplateBuilder AddText(string text)
+    {
+        template.Append($"{text}\n");
+        return this;
+    }
 
-     public TemplateBuilder AddText(string text)
-     {
-          template.Append($"{text}\n");
-          return this;
-     }
-     
-     public TemplateBuilder AddAttribute(string name, string value)
-     {
-          var pos = template.ToString().LastIndexOf('>');
-          template.Insert(pos, $"{name}={value}");
-          return this;
-     }
+    public TemplateBuilder AddAttribute(string name, string value)
+    {
+        var pos = template.ToString().LastIndexOf('>');
+        template.Insert(pos, $" {name}={value} ");
+        return this;
+    }
+
+    public TemplateBuilder AddAttributes(ICollection<KeyValuePair<string, string>> attributes)
+    {
+        var pos = template.ToString().LastIndexOf('>');
+        var attribute = "";
+
+        foreach (var attr in attributes)
+        {
+            attribute = $" {attr.Key}={attr.Value} ";
+            template.Insert(pos, attribute);
+            pos += attribute.Length;
+        }
+        return this;
+    }
 }
