474363f2604d3c5d4d1bfe564a0b82c587131195
anatoly <lager11197@yandex.ru>
2023-01-02 01:16:38 +0300

    feat: add processing of input and textarea tags

--- Templater/Parser/TemplateParser.cs
+++ Templater/Parser/TemplateParser.cs
@@ -1,4 +1,7 @@
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
 
 namespace Templater.Builder;
 
@@ -17,7 +20,10 @@ public class TemplateParser: TemplatePatterns
 
     public string Parse()
     {
+        StringBuilder temp = new ();
+        List<string> keys = new();
         string id, tag, type, title, text;
+        string literalKey, litrealBody;
         
         var literals = _markdown
                 .Split(SPLITER)
@@ -25,9 +31,42 @@ public class TemplateParser: TemplatePatterns
                 .Select(literal => literal.Trim())
                 .ToArray();
 
+        _builder.Clear();
+        
         foreach (var literal in literals)
         {
+            var literalParts = literal.Split(':');
+            literalKey = literalParts[0].Trim();
+            litrealBody = literalParts[1].Trim();
             
+            temp.Clear().Append("\n");
+            title = literalKey;
+            if (!IsNull(ptrMarksArea.Execute(litrealBody, 0).Result))
+            {
+                var markGroup = ptrMarkGroupWords.Matches(litrealBody);
+                tag = (markGroup.Count > 1) ? "textarea" : "input";
+                
+                foreach (Match match in markGroup)
+                {
+                    text = ptrDuoMarkContent.Execute(match.Value, 0).Result;
+                    temp.Append((String.IsNullOrEmpty(text)) ? "\n": $"{text}\n");
+                    _builder
+                        .AddTag("div")
+                            .AddAttribute("class", "form-floating mb-3")
+                            .AddTag(tag)
+                            .AddAttribute("class", "form-control")
+                            .AddAttribute("name", literalKey)
+                            .AddAttribute("id", literalKey)
+                            .AddAttribute("placeholder", title)
+                            .AddText((tag == "input")
+                                ? $" value={temp}>"
+                                : $">{temp} </{tag}>")
+                        .AddTag("label")
+                        .AddAttribute("for", literalKey)
+                        .AddText(title)
+                        .AddTag("/label");
+                }
+            }
         }
         
         return _builder.Build();
@@ -36,5 +75,10 @@ public class TemplateParser: TemplatePatterns
     public string GetMarkdown()
     {
         return _markdown;
+    }
+    
+    private bool IsNull(object? value)
+    {
+        return value == null;
     }
 }
