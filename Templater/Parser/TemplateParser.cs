de56f170d0f99a03b09f337f8e99f47386e0856f
anatoly <lager11197@yandex.ru>
2023-01-04 01:49:39 +0300

    feat: added <select> building

--- Templater/Parser/TemplateParser.cs
+++ Templater/Parser/TemplateParser.cs
@@ -7,7 +7,7 @@ namespace Templater.Builder;
 
 public class TemplateParser: TemplatePatterns
 {
-    private const char SPLITER = ';';
+    private const char LITRAL_SPLITER = ';';
     
     private readonly string _markdown;
     private readonly TemplateBuilder _builder;
@@ -24,23 +24,26 @@ public class TemplateParser: TemplatePatterns
         List<string> keys = new ();
         string id, tag, type, title, text;
         string literalKey, litrealBody;
-        
+
         _builder.Clear();
         
         var literals = _markdown
-                .Split(SPLITER)
+                .Split(LITRAL_SPLITER)
                 .Where(literal => !string.IsNullOrWhiteSpace(literal))
                 .Select(literal => literal.Trim())
                 .ToArray();
         
         foreach (var literal in literals)
         {
+            temp.Clear();
+            
             var literalParts = literal.Split(':');
             literalKey = literalParts[0].Trim();
             litrealBody = literalParts[1].Trim();
             
-            temp.Clear().Append("\n");
+            temp.Append("\n");
             title = literalKey;
+            
             if (!IsNull(ptrMarksArea.Execute(litrealBody, 0).Result))
             {
                 var markGroup = ptrMarkGroupWords.Matches(litrealBody);
@@ -50,27 +53,64 @@ public class TemplateParser: TemplatePatterns
                 {
                     text = ptrDuoMarkContent.Execute(match.Value, 0).Result;
                     temp.Append((String.IsNullOrEmpty(text)) ? "\n": $"{text}\n");
-                    _builder
-                        .AddTag("div")
-                        .AddAttribute("class", "form-floating mb-3")
-                            .AddTag(tag)
-                            .AddAttribute("class", "form-control")
-                            .AddAttribute("name", literalKey)
-                            .AddAttribute("id", literalKey)
-                            .AddAttribute("placeholder", title)
-                            .AddText((tag == "input")
-                                ? $" value={temp.ToString()}>"
-                                : $">{temp.ToString()} </{tag}>")
-                            .AddTag("label")
-                            .AddAttribute("for", literalKey)
-                            .AddText(title)
-                            .AddTag("/label")
-                        .AddTag("/div");
                 }
+                
+                _builder
+                    .AddTag("div")
+                    .AddAttribute("class", "form-floating mb-3")
+                    .AddTag(tag)
+                    .AddAttribute("class", "form-control")
+                    .AddAttribute("name", literalKey)
+                    .AddAttribute("id", literalKey)
+                    .AddAttribute("placeholder", title)
+                    .AddText((tag == "input")
+                        ? $" value={temp}>"
+                        : $">{temp} </{tag}>")
+                    .AddTag("label")
+                    .AddAttribute("for", literalKey)
+                    .AddText(title)
+                    .AddTag("/label")
+                    .AddTag("/div");
             }
             else
             {
+                var options = litrealBody
+                    .Split(',')
+                    .Where(option => !string.IsNullOrWhiteSpace(option))
+                    .Select(option => option.Trim())
+                    .ToArray();
                 
+                if (!IsNull(ptrVerticalBraceArea.Execute(options[0], 0).Result)) 
+                {
+                    foreach(var option in options)
+                    {
+                        _builder
+                            .AddTag("label")
+                            .AddAttribute("for", literalKey)
+                            .AddAttribute("class", "form-label")
+                            .AddText(title)
+                            .AddTag("/label")
+                            .AddTag("select")
+                            .AddAttribute("name", literalKey)
+                            .AddAttribute("id", literalKey)
+                            .AddAttribute("class", "form-select mb-3")
+                            .AddAttribute("aria-label", literalKey);
+
+                        if (!IsNull(ptrVerticalBraceArea.Execute(option, 0).Result))
+                        {
+                            var optionTemplate = ptrVerticalBraceContent.Execute(option, 0);
+                            var selected = (option[optionTemplate.EndPosition + 2] == '*' ? "selected" : "");
+                            _builder
+                                .AddTag("option")
+                                .AddAttribute("value", optionTemplate.Result)
+                                .AddAttribute(selected)
+                                .AddText(optionTemplate.Result)
+                                .AddTag("/option");
+                        }
+
+                        _builder.AddTag("/select");
+                    }
+                }
             }
         }
         
@@ -86,4 +126,5 @@ public class TemplateParser: TemplatePatterns
     {
         return value == null;
     }
+
 }
